a4eee1ba098ca88f6e008644655b5117515c382d
soham kadam <117815216+sohamkadam9@users.noreply.github.com>
2022-11-09 17:20:57 +0530

    server file modified

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-	res.send('hello');
+	res.send('Working fine');
 });
 
 app.listen(PORT, () => {
-	console.log('Server started');
+	console.log(`Server started at ${PORT}`);
 });
